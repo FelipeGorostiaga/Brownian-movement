c0dca36c5f023f786a48c68d9999b1bae345e7fd
FelipeGorostiaga <fgorostiaga@itba.edu.ar>
2020-03-31 23:19:22 -0300

    Big particle position graph

---
+++ graphics/bigParticlePath.py
@@ -0,0 +1,22 @@
+from matplotlib import pyplot as plt
+
+with open('../output.txt', 'r') as f:
+    lines = f.readlines()
+    N = int(lines[0])
+    X = [float(lines[x].split()[1]) for x in range(0, len(lines)) if x % (N + 2) == 2]
+    Y = [float(lines[x].split()[2]) for x in range(0, len(lines)) if x % (N + 2) == 2]
+    plt.style.use('ggplot')
+    plt.plot(X, Y, color='red', )
+    plt.axis([0,0.5,0,0.5])
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    plt.title('Big particle position')
+    plt.show()
+
+
+
+
+
+
+
+
